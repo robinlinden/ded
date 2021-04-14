58a0fcf69521bd37e45fbf6089fd0b207c9dff55
Robin Linden <dev@robinlinden.eu>
2021-04-14 20:12:35 +0200

    Add contact selection

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, input, li, text, ul)
+import Html exposing (Html, button, div, input, li, p, text, ul)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onClick, onInput)
 
@@ -12,17 +12,18 @@ main =
 
 
 type alias Model =
-    { contacts : List String, contactInput : String }
+    { contacts : List String, contactInput : String, selectedContact : String }
 
 
 init : Model
 init =
-    { contacts = [], contactInput = "" }
+    { contacts = [], contactInput = "", selectedContact = "" }
 
 
 type Msg
     = ContactInputChange String
     | AddContact
+    | SelectContact String
 
 
 update : Msg -> Model -> Model
@@ -34,11 +35,15 @@ update msg model =
         AddContact ->
             { model | contacts = model.contacts ++ [ model.contactInput ], contactInput = "" }
 
+        SelectContact contact ->
+            { model | selectedContact = contact }
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ input [ placeholder "contact", value model.contactInput, onInput ContactInputChange ] []
         , button [ onClick AddContact ] [ text "Add" ]
-        , ul [] (List.map (\l -> li [] [ text l ]) model.contacts)
+        , ul [] (List.map (\l -> li [ onClick (SelectContact l) ] [ text l ]) model.contacts)
+        , p [] [ text ("Selected contact: " ++ model.selectedContact) ]
         ]
