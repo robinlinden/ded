7260a7fb682d0ce86290818ac4a4e9b96f1037a6
Robin Linden <dev@robinlinden.eu>
2021-04-14 20:43:19 +0200

    Add chat support

--- src/Main.elm
+++ src/Main.elm
@@ -15,6 +15,8 @@ type alias Model =
     { contacts : List String
     , contactInput : String
     , selectedContact : String
+    , messages : List ( String, String )
+    , messageInput : String
     }
 
 
@@ -23,6 +25,8 @@ init =
     { contacts = []
     , contactInput = ""
     , selectedContact = ""
+    , messages = []
+    , messageInput = ""
     }
 
 
@@ -30,6 +34,8 @@ type Msg
     = ContactAdded
     | ContactInputUpdated String
     | ContactSelected String
+    | MessageInputUpdated String
+    | MessageSent
 
 
 update : Msg -> Model -> Model
@@ -47,6 +53,15 @@ update msg model =
         ContactSelected contact ->
             { model | selectedContact = contact }
 
+        MessageInputUpdated new ->
+            { model | messageInput = new }
+
+        MessageSent ->
+            { model
+                | messages = model.messages ++ [ ( model.selectedContact, model.messageInput ) ]
+                , messageInput = ""
+            }
+
 
 view : Model -> Html Msg
 view model =
@@ -63,5 +78,16 @@ view model =
             ]
             [ text "add" ]
         , ul [] (List.map (\l -> li [ onClick (ContactSelected l) ] [ text l ]) model.contacts)
-        , p [] [ text ("selected contact: " ++ model.selectedContact) ]
+        , p [] [ text ("talking to " ++ model.selectedContact) ]
+        , ul []
+            (model.messages
+                |> List.filter (\m -> Tuple.first m == model.selectedContact)
+                |> List.map (\l -> li [] [ text (Tuple.second l) ])
+            )
+        , input [ placeholder "message", value model.messageInput, onInput MessageInputUpdated ] []
+        , button
+            [ disabled (String.isEmpty model.selectedContact || String.isEmpty model.messageInput)
+            , onClick MessageSent
+            ]
+            [ text "send" ]
         ]
